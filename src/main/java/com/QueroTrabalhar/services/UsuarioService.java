ba333af8250a20d247e7f1369bf27e1656647e76
joselucio <jose.lourenco@dcx.ufpb.br>
2026-08-21 13:39:55 -0300

    Test Tolken

--- src/main/java/com/QueroTrabalhar/services/UsuarioService.java
+++ src/main/java/com/QueroTrabalhar/services/UsuarioService.java
@@ -21,6 +21,7 @@ import java.util.stream.Collectors;
 @Service
 public class UsuarioService implements UserDetailsService {
 
+// TEst Test Test
 
     @Autowired
     private UsuarioRepository usuarioRepository;
